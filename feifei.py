46641aef84c43f3460bf184662dbc7a004a3cef4
likangfei <lkf_01@163.com>
2018-08-04 12:51:53 +0800

    change

---
+++ feifei.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#####################################
+# File name : feifei.py
+# Create date : 2018-08-04 12:47
+# Modified date : 2018-08-04 12:49
+# Author : DARREN
+# Describe : not set
+# Email : lzygzh@126.com
+#####################################
+
+def hello():
+    print "hello world"
+
+hello()
